bfb2448fb951f4ba68b2b11fe2a1254edb844405
olliez-mods <socksinthewash@gmail.com>
2024-03-14 22:05:05 +1100

    creating dummy

---
+++ Server/server.py
@@ -0,0 +1,15 @@
+# Big server, this manges connecting clients and client_hosts and connects them
+
+import socket
+
+ip = "0.0.0.0"
+port = "30814"
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((ip, port))
+server.listen(5)
+
+client_socket, address = server.accept()
+print("GOTTEM")
+byts = client_socket.recv(1024)
+print(byts.decode())

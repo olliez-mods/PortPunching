1a700da301ef6ebe32c2847d3dfed4694d022214
olliez-mods <socksinthewash@gmail.com>
2024-03-14 22:10:58 +1100

    Fixed port being string

--- DummyClient/dummy.py
+++ DummyClient/dummy.py
@@ -2,9 +2,10 @@
 import socket
 
 code = "123"
-big_ip = "121.223.133.83"
-big_port = "30814"
+#big_ip = "121.223.133.83"
+big_ip = "69.124.197.126"
+big_port = 30814
 
 big_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 big_server.connect((big_ip, big_port))
-big_server.send("IM HERE".encode())
+big_server.send("connect".encode())
